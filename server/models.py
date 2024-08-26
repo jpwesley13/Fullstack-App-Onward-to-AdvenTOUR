0e4fc701d13056134fc56c6ed766f5a611e0b03d
Jared Patterson <jpwesley34@gmail.com>
2024-08-26 13:28:46 -0700

    Add validations to models.

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
@@ -12,6 +13,20 @@ class Habitat(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False, unique=True)
     danger = db.Column(db.Integer, nullable=False)
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Habitat must be named.')
+        elif not 25 <= len(name) < 1:
+            raise ValueError('Habitat names must be between 2-25 characters long')
+        return name
+    
+    @validates('danger')
+    def validate_danger(self, key, danger):
+        if not danger:
+            raise ValueError('Please enter the observed danger levels of this habitat.')
+        return danger
+
     def __repr__(self):
         return f'<Habitat {self:id}: {self.name}, danger: {self.danger}>'
     
@@ -22,6 +37,18 @@ class Trainer(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False, unique=True)
     age = db.Column(db.Integer)
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Please enter a name.')
+        return name
+    
+    @validates('age')
+    def validate_age(self, key, age):
+        if not age >= 10:
+            raise ValueError('Trainers must be at least 10 years old.')
+        return age
+
     def __repr__(self):
         return f'<Trainer {self.id}: {self.name}, age: {self.age}>'
     
@@ -29,3 +56,5 @@ class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
+    habitat_id = db.Column(db.Integer, db.ForeignKey('habitats.id'))
+    traier_id = db.Column(db.Integer, db.ForeignKey('trainers.id'))
