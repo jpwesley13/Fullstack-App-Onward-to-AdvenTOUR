f90019d97934b75075d480794ba2ad29f26b23ba
Jared Patterson <jpwesley34@gmail.com>
2024-08-29 13:09:15 -0700

    Adjust seed file for added regions.

--- server/seed.py
+++ server/seed.py
@@ -8,7 +8,7 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, Trainer, Habitat, Review
+from models import db, Trainer, Habitat, Review, Region
 
 if __name__ == '__main__':
     fake = Faker()
@@ -17,6 +17,19 @@ if __name__ == '__main__':
         # Seed code goes here!
         Trainer.query.delete()
         Habitat.query.delete()
+        Review.query.delete()
+        Region.query.delete()
+        
+        regions = []
+        region_names = ["Kanto", "Johto", "Hoenn", "Sinnoh", "Unova", "Kalos", "Alola", "Galar", "Paldea", "Orre", "Ultra Space", "Fiore", "Almia", "Oblivia", "Lental", "Uncharted"]
+
+        for region_name in region_names:
+            region = Region(
+                name = region_name
+            )
+            regions.append(region)
+        
+        db.session.add_all(regions)    
 
         trainers = []
         names = []
@@ -46,6 +59,8 @@ if __name__ == '__main__':
                 name = fake.city(),
                 danger = randint(1,5)
             )
+
+            habitat.region = rc(regions)
 
             habitats.append(habitat)
 
