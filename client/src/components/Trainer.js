a41a93b23a909f01ba029211ea981b07857c4fed
Jared Patterson <jpwesley34@gmail.com>
2024-09-05 15:38:41 -0700

    Adds trainers to Contributors page.

--- client/src/components/Trainer.js
+++ client/src/components/Trainer.js
@@ -1,10 +1,32 @@
-
+import { useOutletContext, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Trainer() {
+
+    const [trainers, setTrainers] = useState([]);
+
+    useEffect(() => {
+        fetch('/trainers')
+        .then(res => res.json())
+        .then(data => {
+            setTrainers(data);
+        })
+        .catch(error => console.error(error));
+    }, []);
+
+    useEffect(() => {
+        document.title = "Contributors"
+    }, [])
+
+    const trainerList = trainers.map(trainer => (
+        <li>{trainer.name}: <Link to={`/trainers/${trainer.id}`}>Visit Profile</Link></li>
+    ))
+
     return (
         <>
         <h2>Contributors</h2>
         <br/>
+        {trainerList}
         </>
     );
 };
