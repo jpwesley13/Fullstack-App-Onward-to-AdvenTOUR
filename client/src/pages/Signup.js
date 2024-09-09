ddc386d741c95e7b88996129d0d44c4acd53c9d6
Jared Patterson <jpwesley34@gmail.com>
2024-09-09 01:44:30 -0700

    Add signup options and basic css to form.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,8 +8,9 @@ function Signup() {
             name: "",
             age: "",
             image: "",
+            biome: "",
             password: "",
-            confirm: ""
+            confirmPassword: ""
         }
     });
     return (
@@ -21,6 +22,44 @@ function Signup() {
                 id="name" 
                 type="name" 
                 placeholder="Enter your name" />
+            <label htmlFor="age">Age</label>
+            <input
+                value={formik.values.age}
+                onChange={formik.handleChange}
+                id="age" 
+                type="number" 
+                placeholder="Enter your age" />
+            <label htmlFor="image">Profile Picture</label>
+            <input
+                value={formik.values.image}
+                onChange={formik.handleChange}
+                id="image" 
+                type="text" 
+                placeholder="Post your profile picture's url" />
+            <label htmlFor="biome">Favorite Biome</label>
+            <input
+                value={formik.values.biome}
+                onChange={formik.handleChange}
+                id="biome" 
+                type="text" 
+                placeholder="Enter your favorite biome" />
+            <label htmlFor="password">Password</label>
+            <input
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                id="password" 
+                type="password" 
+                placeholder="Enter your password" />
+            <label htmlFor="password">Confirm Password</label>
+            <input
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                id="confirmPassword" 
+                type="password" 
+                placeholder="Confirm your password" />
+                <button type="submit">Submit</button>
         </form>
     )
 };
