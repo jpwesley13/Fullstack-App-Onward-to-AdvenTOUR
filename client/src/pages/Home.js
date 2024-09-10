46314f52657024f7209922dec39a7dc7042c650d
Jared Patterson <jpwesley34@gmail.com>
2024-09-10 03:46:28 -0700

    Add filtering by region to home habitats page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,22 @@
 import { useOutletContext } from "react-router-dom";
 import HabitatCard from "../components/HabitatCard";
 import { useState, useEffect } from "react";
+import RegionFilter from "../components/RegionFilter";
 
 function Home() {
 
     const [habitats, setHabitats] = useState([]);
+    const [filteredHabitats, setFilteredHabitats] = useState([]);
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
       document.title = "Onward to AdvenTOUR"
       fetch('/habitats')
       .then(res => res.json())
-      .then(data => setHabitats(data))
+      .then(data => {
+        setHabitats(data)
+        setFilteredHabitats(data)
+      })
       .catch(error => console.error(error));
       fetch('/reviews')
       .then(res => res.json())
@@ -29,18 +34,25 @@ function Home() {
       return average
   }
 
-    const displayedHabitats = habitats.map(habitat => (
+    const displayedHabitats = filteredHabitats.map(habitat => (
       <HabitatCard 
       key={habitat.id}
       habitat={habitat}
       dangerAverage={dangerAverage}
       />
     ))
+    
+    const regions = [...new Set(habitats.map(habitat => habitat.region.name))]
+
 
     return (
         <>
           <hr/>
           <h2 className="header">Habitats:</h2>
+          <RegionFilter
+          regions={regions}
+          habitats={habitats}
+          setFilteredHabitats={setFilteredHabitats}/>
           {displayedHabitats}
         </>
     )
