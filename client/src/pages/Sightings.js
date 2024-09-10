3abeb61c10a6401f11507aa8d93a0ec2616bb47e
Jared Patterson <jpwesley34@gmail.com>
2024-09-10 06:06:11 -0700

    Finalize filter logic across pages.

--- client/src/pages/Sightings.js
+++ client/src/pages/Sightings.js
@@ -1,32 +1,45 @@
 import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SightingCard from "../components/SightingCard";
+import FilterCard from "../components/FilterCard";
 
 function Sightings() {
 
     const [sightings, setSightings] = useState([]);
+    const [filteredSightings, setFIlteresSightings] = useState([]);
 
     useEffect(() => {
         document.title = "Rare Sightings"
         fetch('/sightings')
         .then(res => res.json())
-        .then(data => setSightings(data))
+        .then(data => {
+            setSightings(data)
+            setFIlteresSightings(data);
+        })
         .catch(error => console.error(error));
       }, []);
 
     // const {sightings} = useOutletContext();
 
-    const displayedSightings = sightings.map(sighting => (
+    const displayedSightings = filteredSightings.map(sighting => (
         <SightingCard 
         key={sighting.id}
         sighting={sighting}
         />
       ))
 
+    const habitats = [...new Set(sightings.map(sighting => sighting.habitat.name))]
+
     return (
         <>
         <hr/>
         <h2>Confirmed rare sightings:</h2>
+        <FilterCard
+        specifics={habitats}
+        broadGroups={sightings}
+        setFilteredGroups={setFIlteresSightings}
+        label="rare sightings"
+        filterBy="habitat" />
         <br/>
         {displayedSightings}
         </>
