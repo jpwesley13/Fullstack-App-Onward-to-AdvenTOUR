292e653d884100ffd16d5b098bc419aaffd4dcd9
Jared Patterson <jpwesley34@gmail.com>
2024-09-16 02:21:41 -0700

    Adjust ErrorPage to allow navigation back to main site.

--- client/src/pages/ErrorPage.js
+++ client/src/pages/ErrorPage.js
@@ -1,4 +1,5 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, NavLink } from "react-router-dom";
+import Header from "../components/Header";
 
 function ErrorPage() {
   const error = useRouteError();
@@ -6,6 +7,29 @@ function ErrorPage() {
 
   return (
     <>
+    <header>
+    <nav className="navbar">
+            <div className="navbutton">
+                <NavLink
+                to="/"
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                to="/trainers"
+                >
+                    Contributors
+                </NavLink>
+                <NavLink
+                to="/sightings"
+                >
+                    Rare Sightings
+                </NavLink>
+            </div>
+    </nav>
+    </header>
+    <Header />
+    <hr />
       <main>
         <h1>You're in deep, uncharted territory now. Better play it safe and try another page!</h1>
       </main>
